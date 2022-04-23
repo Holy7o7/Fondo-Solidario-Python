ffc85498d5aa13b84cffbaee1edd9aa5be445341
Nicolas Saldivia <64153626+Holy7o7@users.noreply.github.com>
2022-04-23 00:08:19 -0400

    se añade base de datos de prueba

---
+++ beta_01.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 8.0.28, for Linux (x86_64)
+--
+-- Host: localhost    Database: beta_01
+-- ------------------------------------------------------
+-- Server version	8.0.28-0ubuntu0.20.04.3
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `indicators`
+--
+
+DROP TABLE IF EXISTS `indicators`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `indicators` (
+  `id_indicador` int NOT NULL,
+  `nombre_indicador` varchar(45) DEFAULT NULL,
+  `progreso` int DEFAULT NULL,
+  `mes` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_indicador`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `indicators`
+--
+
+LOCK TABLES `indicators` WRITE;
+/*!40000 ALTER TABLE `indicators` DISABLE KEYS */;
+INSERT INTO `indicators` VALUES (1,'Indicador01',5,'Enero'),(2,'Indicador01',5,'Febrero'),(3,'Indicador01',6,'Marzo'),(4,'Indicador01',6,'Abril'),(5,'Indicador02',4,'Enero'),(6,'Indicador02',4,'Febrero'),(7,'Indocador02',5,'Marzo'),(8,'Indicador02',6,'Abril'),(9,'Indicador03',6,'Enero'),(10,'Indicador03',7,'Febrero'),(11,'Indicador03',7,'Marzo'),(12,'Indicador03',8,'Abril');
+/*!40000 ALTER TABLE `indicators` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-04-22 23:21:50
