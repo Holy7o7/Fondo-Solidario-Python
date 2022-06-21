99749f3d63aa95afb39c230c65d45928ae9ecc74
Nicolas Saldivia <64153626+Holy7o7@users.noreply.github.com>
2022-06-20 22:06:58 -0400

    implementacion clase

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-import matplotlib.pyplot as plt
-import pandas as pd 
-import csv
-
+from clase.graficos import *
 
 from flask import Flask, render_template
 
@@ -10,32 +7,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def graf():
-    plt.style.use('ggplot')
-
-    list_indicadores = pd.read_csv('indicador.csv', index_col=None)
 
     #print(list_indicadores)
 
     #grafico de Barras General
 
-    list_indicadores.plot(x = 'nombre_indicador', y = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'], kind='bar' ,title = 'Progreso indicadores Mes a Mes')
-    plt.gcf().subplots_adjust(bottom= 0.20)
-    plt.xlabel('Indicadores')
-    plt.ylabel('Progreso')
-    plt.xticks(rotation = 45, horizontalalignment = 'center')
-    plt.savefig('static/images/barG_chart.svg')
+    graficoG = graficos("Progreso indicadores general", "Febrero")
+    graficoG.crearGrafico()
     barG_img = 'static/images/barG_chart.svg'
 
     #grafico de Barras mes enero
 
-    list_indicadores.plot(x = 'nombre_indicador', y = 'Enero' , kind='bar' ,title = 'Progreso indicadores Mes de Enero')
-    plt.gcf().subplots_adjust(bottom= 0.20)
-    plt.xlabel('Indicadores')
-    plt.ylabel('Progreso')
-    plt.xticks(rotation = 45, horizontalalignment = 'center')
-    plt.savefig('static/images/barE_chart.svg')
-    barE_img = 'static/images/barE_chart.svg'
-    return render_template('index.html', barG_url = barG_img, barE_url = barE_img)
+    #list_indicadores.plot(x = 'nombre_indicador', y = 'Enero' , kind='bar' ,title = 'Progreso indicadores Mes de Enero')
+    #plt.gcf().subplots_adjust(bottom= 0.20)
+    #plt.xlabel('Indicadores')
+    #plt.ylabel('Progreso')
+    #plt.xticks(rotation = 45, horizontalalignment = 'center')
+    #plt.savefig('static/images/barE_chart.svg')
+    #barE_img = 'static/images/barE_chart.svg'
+    return render_template('index.html', barG_url = barG_img)
 
 if __name__ == '__main__':
    app.run(debug = True)
---
+++ clase/graficos.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import csv 
+
+class graficos:
+    def __init__(self, titulo, mes):
+        self.indicadores = pd.read_csv('indicador.csv', index_col=None)
+        self.ejeX = "Indicadores"
+        self.ejeY = "Progreso"
+        self.titulo = titulo
+
+        if(mes == "general"):
+            column = self.indicadores.columns.values
+            mes = np.delete(column,0)
+        self.mes = mes
+
+    def crearGrafico(self):
+        plt.style.use('ggplot')
+        self.indicadores.plot(x = 'nombre_indicador', y = self.mes, kind='bar' ,title = self.titulo)
+        plt.gcf().subplots_adjust(bottom= 0.20)
+        plt.xlabel(self.ejeX)
+        plt.ylabel(self.ejeY)
+        plt.xticks(rotation = 45, horizontalalignment = 'center')
+        plt.savefig('static/images/barG_chart.svg')
